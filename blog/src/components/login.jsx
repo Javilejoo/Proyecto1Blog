e616238729b76f0d64b5220d70a056696693bf2a
Javilejo <pra21486@uvg.edu.gt>
2024-04-30 00:48:24 -0600

    [ADD] implementacion de useForm
    se implemento useForm para la autenticación del login y su funcionamiento

--- blog/src/components/login.jsx
+++ blog/src/components/login.jsx
@@ -1,15 +1,27 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import Button from '@mui/material/Button';
+
 function Login() {
-  const user = "admin";
-  const password = "admin";
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    if (data.username === 'admin' && data.password === 'admin') {
+      // Si el usuario y la contraseña son "admin", navegar al admin
+      window.location.href = '/admin';
+    } else {
+      // Si no, mostrar un mensaje de error
+      alert('Usuario o contraseña incorrectos');
+    }
+  };
 
   return (
     <div>
-      <h1> Login</h1>
-      <form>
-        <input type="text" placeholder="Usuario"></input>
-        <input type="password" placeholder="Contraseña"></input>
-        <Button variant="contained" href="/admin" color="primary">Iniciar sesión</Button>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Usuario" {...register('username')} />
+        <input type="password" placeholder="Contraseña" {...register('password')} />
+        <Button type="submit" variant="contained" color="primary">Iniciar sesión</Button>
       </form>
     </div>
   );
