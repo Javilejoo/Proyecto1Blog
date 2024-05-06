d955c9261cd1b3b79ae0ae8c7442f3d5099a101d
Javilejoo <pra21486@uvg.edu.gt>
2024-05-06 01:27:38 -0600

    [ADD] delete usando useApi

--- blog/src/components/deleteCharacter.jsx
+++ blog/src/components/deleteCharacter.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import useApi from './useApi'; // Importa el hook useApi
+
 function DeleteCharacter({ id, handleDelete }) {
+    const { deleteData } = useApi(); // Usa el hook useApi
+
     const deleteCharacter = async () => {
         try {
-            await fetch(`http://127.0.0.1:3010/posts/${id}`, {
-                method: 'DELETE',
-            });
+            await deleteData(`http://127.0.0.1:3010/posts/${id}`);
             handleDelete(); // Llama a la función de manejo después de la eliminación
         } catch (error) {
             console.error('Error deleting character:', error);
